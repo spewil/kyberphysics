92f7783660294e46b958f19ce002151cd6946cb2
glopesdev <glopesdev@users.noreply.github.com>
2020-09-28 11:40:29 +0100

    Add first working draft of #3

---
+++ src/python/arcreach.py
@@ -0,0 +1,37 @@
+"""Small example OSC client
+
+This program sends 10 random values between 0.0 and 1.0 to the /filter address,
+waiting for 1 seconds between each value.
+"""
+import argparse
+import random
+import time
+from pythonosc import udp_client
+from pythonosc import dispatcher
+from pythonosc import osc_server
+import asyncio
+import numpy as np
+
+def print_volume_handler(unused_addr, args, volume):
+  print("[{0}] ~ {1}".format(args[0], volume))
+
+def print_compute_handler(unused_addr, args, volume):
+  try:
+    print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+  except ValueError: pass
+
+def default_handler(address, *args):
+    print(f"DEFAULT {address}: {args}")
+
+dispatcher = dispatcher.Dispatcher()
+dispatcher.set_default_handler(default_handler)
+
+client = udp_client.SimpleUDPClient("127.0.0.1", 5005)
+with osc_server.BlockingOSCUDPServer(("127.0.0.1", 5006), dispatcher) as server:
+    
+    for i in range(10):
+        side = np.random.randint(2) * 2.0 - 1 
+        client.send_message("/start", [side, 1.0, 0.6, 0.8, 5000, 500, 500, 5000])
+        server.handle_request()
+        time.sleep(1)
+        
