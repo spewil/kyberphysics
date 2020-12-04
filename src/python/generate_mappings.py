19f1018d1c58e58506859837ebe88ee7ffa60239
spewil <spencer.wilson@ucl.ac.uk>
2020-12-04 19:43:52 +0000

    Fixed cursor script memory issue

--- src/python/generate_mappings.py
+++ src/python/generate_mappings.py
@@ -1,12 +1,15 @@
 import numpy as np 
 
 numchannels = 32
+state_dimensionality = 6
 scale_factor = 0 
 
-dynamics = scale_factor*np.eye(6,dtype=np.float32)
+dynamics = scale_factor*np.eye(state_dimensionality,dtype=np.float32)
 
-decoder = np.zeros(shape=(6,numchannels),dtype=np.float32)
-decoder[2,0] = 1
+channel_to_visualize = 12
+state_dim_to_place_channel = 0
+decoder = np.zeros(shape=(state_dimensionality,numchannels),dtype=np.float32)
+decoder[state_dim_to_place_channel,channel_to_visualize] = 1
 
 with open("dynamics.bin","wb") as file:
 	file.write(dynamics.tobytes())
